f4a0e0d8feb19c9077dfec7ca85e14c3416929f1
Nicolas Gross <nicolas@fam-gross.org>
2023-03-15 19:26:05 +0100

    Add aarch64 compiled LLVM IR generation to script

--- DEVELOPING_OPAL/validate/src/test/resources/llvm/cross_language/taint/aarch64/build.sh
+++ DEVELOPING_OPAL/validate/src/test/resources/llvm/cross_language/taint/aarch64/build.sh
@@ -14,8 +14,8 @@ if [[ "$gcc_target" != *"${target_comps[0]}"* || "$gcc_target" != *"${target_com
 fi
 
 # compiled LLVM IR is basically equivalent to x86_64
-#clang-15 -S -I/usr/lib/jvm/default-java/include/ -I/usr/lib/jvm/default-java/include/linux/ -emit-llvm -O0 -o TaintTest_O0.ll -target "$target" ../TaintTest.c
-#clang-15 -S -I/usr/lib/jvm/default-java/include/ -I/usr/lib/jvm/default-java/include/linux/ -emit-llvm -O2 -o TaintTest_O2.ll -target "$target" ../TaintTest.c
+clang-15 -S -I/usr/lib/jvm/default-java/include/ -I/usr/lib/jvm/default-java/include/linux/ -emit-llvm -O0 -o TaintTest_O0.ll -target "$target" ../TaintTest.c
+clang-15 -S -I/usr/lib/jvm/default-java/include/ -I/usr/lib/jvm/default-java/include/linux/ -emit-llvm -O2 -o TaintTest_O2.ll -target "$target" ../TaintTest.c
 
 clang-15 -shared -fpic -I/usr/lib/jvm/default-java/include/ -I/usr/lib/jvm/default-java/include/linux/ -O0 -o libtainttest_clang_O0.so -target "$target" ../TaintTest.c
 clang-15 -shared -fpic -I/usr/lib/jvm/default-java/include/ -I/usr/lib/jvm/default-java/include/linux/ -O2 -o libtainttest_clang_O2.so -target "$target" ../TaintTest.c
